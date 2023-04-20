b6dc1ecdcb15ba0c39c367df9c78c5abc9145d14
Krystian Wiese <75091948+k-wiese@users.noreply.github.com>
2023-04-20 19:19:43 +0200

    created navbar

---
+++ resources/views/livewire/navbar.blade.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    
+          <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+            <ul class="navbar-nav">
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Products Example App</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+</div>
