5a77d221648bcdac3af45e7854481ecb0949e6d5
Krystian Wiese <75091948+k-wiese@users.noreply.github.com>
2023-04-25 13:03:13 +0200

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PriceController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,15 +18,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    
+    Route::get('/dashboard', function(){
+        return redirect()->route('home');
+    });
+    
+    Route::resource('/product', ProductController::class);
+    route::resource('/price', PriceController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
